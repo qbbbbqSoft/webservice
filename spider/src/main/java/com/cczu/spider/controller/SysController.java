cd4c5a1444077abcb5d27f9c6807e020b7bd50fd
qbbbbqSoft <807116433@qq.com>
2018-10-31 21:02:02 +0800

    20181031

--- spider/src/main/java/com/cczu/spider/controller/SysController.java
+++ spider/src/main/java/com/cczu/spider/controller/SysController.java
@@ -1,20 +1,23 @@
 package com.cczu.spider.controller;
 
 import com.cczu.spider.entity.SysSuggestionEntity;
+import com.cczu.spider.entity.SysZoneEntity;
 import com.cczu.spider.repository.SysSuggestionRepo;
+import com.cczu.spider.repository.SysZoneRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Example;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/sys")
 public class SysController {
     @Autowired
     private SysSuggestionRepo sysSuggestionRepo;
+    @Autowired
+    private SysZoneRepo sysZoneRepo;
 
     @RequestMapping(value = "/wxapi")
     public String checkWeixinValid(@RequestParam(name="signature")String signature,
@@ -31,6 +34,15 @@ public class SysController {
         if (save != null) {
             return true;
         }
+        return false;
+    }
+
+    @RequestMapping("/queryZoneCode/{zoneCode}")
+    public boolean queryZoneCode(@PathVariable("zoneCode")String zoneCode) {
+        SysZoneEntity entity = new SysZoneEntity();
+        entity.setZonecode(zoneCode);
+        Example example = Example.of(entity);
+        Optional one = sysZoneRepo.findOne(example);
         return false;
     }
 }
